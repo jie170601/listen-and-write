282d00d7b26e1217427874382140c9bcfcb44fec
= <=>
2019-09-02 22:13:16 +0800

    添加了找回单词，单词意思页面

--- src/miniprogram/pages/setting/setting.js
+++ src/miniprogram/pages/setting/setting.js
@@ -2,6 +2,15 @@
 Page({
     data: {},
     onLoad() {
-    }
+    },
+    question_1(){
+      wx.navigateTo({
+        url: '../found/found',
+      })
+  },
+  question_2() {
+    wx.navigateTo({
+      url: '../found/found',
+    })
+  }
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2V0dGluZy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInNldHRpbmcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLElBQUksQ0FBQztJQUNILElBQUksRUFBRSxFQUNMO0lBQ0QsTUFBTTtJQUNOLENBQUM7Q0FDRixDQUFDLENBQUEiLCJzb3VyY2VzQ29udGVudCI6WyJQYWdlKHtcclxuICBkYXRhOiB7XHJcbiAgfSxcclxuICBvbkxvYWQoKSB7XHJcbiAgfVxyXG59KSJdfQ==
--- src/miniprogram/pages/setting/setting.ts
+++ src/miniprogram/pages/setting/setting.ts
@@ -2,5 +2,19 @@ Page({
   data: {
   },
   onLoad() {
+  },
+  question_1(){
+    wx.navigateTo({
+      url: '../found/found',
+    })
+  },
+  question_2() {
+
+  },
+  question_3() {
+
+  },
+  question_4() {
+
   }
 })
