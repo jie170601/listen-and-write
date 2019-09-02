a3fc61c46ef21f9154c57604137bdab71cbcb851
= <=>
2019-09-02 21:50:14 +0800

    完成了设置页面的UI

--- src/miniprogram/pages/setting/setting.js
+++ src/miniprogram/pages/setting/setting.js
@@ -1,2 +1,7 @@
 "use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2V0dGluZy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInNldHRpbmcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbIiJdfQ==
+Page({
+    data: {},
+    onLoad() {
+    }
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2V0dGluZy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInNldHRpbmcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLElBQUksQ0FBQztJQUNILElBQUksRUFBRSxFQUNMO0lBQ0QsTUFBTTtJQUNOLENBQUM7Q0FDRixDQUFDLENBQUEiLCJzb3VyY2VzQ29udGVudCI6WyJQYWdlKHtcclxuICBkYXRhOiB7XHJcbiAgfSxcclxuICBvbkxvYWQoKSB7XHJcbiAgfVxyXG59KSJdfQ==
